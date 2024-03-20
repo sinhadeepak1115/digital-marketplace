9464c5d8914287482040c31e75ca5f95a7ea990a
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-03-20 16:11:19 +0530

    added buttons using shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
+import Link from "next/link";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1 className="text-4xl font-bold tracking-tight">
-          Your marketplace for hight-quality{" "}
-          <span className="text-blue-600">digital assets</span>
-        </h1>
-      </div>
-    </MaxWidthWrapper>
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your marketplace for high quality{" "}
+            <span className="text-violet-600">digital assets</span>
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to DigitalHippo. Every asset on our marketplace is
+            handpicked by our team, ensuring that you have access to the best
+            quality digital assets.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/products" className={buttonVariants()}>
+              Browse Trending
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </>
   );
 }
